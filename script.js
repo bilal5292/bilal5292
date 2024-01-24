0a2ed856976bf69fc56d176966bbf88ff7e1eef3
syed-bilalahmed <syedbilalahmed5292@gmail.com>
2024-01-24 13:05:14 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+// let heading = document.getElementById("heading");
+// heading.innerText = "Salam_Pakistan";
+// script.js
+let heading = document.getElementById("heading");
+heading.style.backgroundColor = "green";
+heading.innerText = 123;
+;
+
+
+// document.getElementById("heading").innerText="Sal";
