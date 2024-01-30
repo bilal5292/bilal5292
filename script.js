77a4cd69ff295e3f775f2a0a0addca9381cc6470
syed-bilalahmed <syedbilalahmed5292@gmail.com>
2024-01-30 12:57:49 +0500

    Initial commit

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
+// let h1=document.querySelector("h1").textContent;
+// let h2 = document.querySelector("h2").textContent;
+
+//   let New= document.querySelector("h1").textContent = h1+" "+h2;
+// let divs =document.querySelectorAll(".box");
+// let idx=1;
+// // for(div of divs){
+// //   div.innerText =`new unique value for ${idx}`;
+// //   idx++;
+// // }
+
+// for(var i=0; i<divs.length; i){
+//   divs[i].innerText =`new unique value for ${idx}`;
+// }
+let divs = document.querySelectorAll(".box");
+let idx = 1;
+for (var i = 0; i < divs.length; i++) {
+  divs[i].innerText = `new unique value for ${idx}`;
+
+  if(idx%2===0){
+    divs[i].style.backgroundColor = "orange";
+  }else {
+    divs[i].style.backgroundColor = "green";
+  }
+  idx++;
+}
+// document.querySelectorAll(".box").forEach(box => {
+//   box.style.backgroundColor = "green";
+// });
+
 // script.js
 // let heading = document.getElementById("heading");
 // heading.innerText = "Salam_Pakistan";
 // script.js
-let heading = document.getElementById("heading");
-heading.style.backgroundColor = "green";
-heading.innerText = 123;
-;
+// let heading = document.getElementById("heading");
+// heading.style.backgroundColor = "green";
+// heading.innerText = 123;
+// ;
 
 
 // document.getElementById("heading").innerText="Sal";
